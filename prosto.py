faa95c3b239f5f85b7e5b5a5cd9563c3e5741835
Caspar Nijhuis <caspar@rhapsody.arnhem.chello.nl>
2017-07-01 19:10:52 +0200

    trying to avoid square root

--- prosto.py
+++ prosto.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from math import sqrt
+from math import sqrt, ceil
 from decimal import *
 from datetime import datetime
 
@@ -14,23 +14,31 @@ def isqrt(n):
 
 def getfactorization(bignumber):
     print "Factorizing %d ..." % bignumber
-    f = 0
-    maximumlimit = isqrt(4 * bignumber)
-    while f < maximumlimit:
+    f = 10000000000
+    f = 0000000000
+    nextsquare = Decimal(4 * bignumber + f * f).sqrt().to_integral_exact(rounding=ROUND_CEILING)
+
+    if nextsquare % 2 == 1:
+        nextsquare = nextsquare + 1
+
+    realsquare = nextsquare * nextsquare
+    limit = isqrt(4 * bignumber)
+
+    while f < limit:
         if f % 100000 == 0:
              print "F: %d" % f
              print datetime.now()
 
         f = f + 2
-        aplusb = Decimal(4 * bignumber + f * f).sqrt()
-        #print aplusb
-        #if f * f + 4 * bignumber == aplusb * aplusb:
-        if aplusb == int(aplusb):
-             #print "YAY %d" % f
-             #print "APLUSB %d" % aplusb
+        tocompare = f * f + 4 * bignumber
+
+        if tocompare == realsquare:
+             aplusb = Decimal(4 * bignumber + f * f).sqrt()
              print "A: %d B: %d" % ((aplusb - f) / 2, (aplusb + f) / 2)
              break
-
+        elif tocompare > realsquare:
+            nextsquare = nextsquare + 2
+            realsquare = nextsquare * nextsquare
 
 getfactorization(197 * 251)
 getfactorization(15331738237 * 15331750531)
